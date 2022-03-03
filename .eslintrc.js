03f31865728e7c5c21565e4191bac7845ff95798
yinhao12138 <2531134419@qq.com>
2022-03-03 14:56:02 +0800

    测试

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,11 @@ module.exports = {
     node: true
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
     'plugin:prettier/recommended'
   ],
   parserOptions: {
@@ -19,6 +19,6 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  }
 }
